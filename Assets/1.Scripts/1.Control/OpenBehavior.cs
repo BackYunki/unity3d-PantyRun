50e8e696aeb224612a788b3f7d2482b02606dbd6
BackYunki <37071344+BackYunki@users.noreply.github.com>
2018-04-26 18:52:54 +0900

    음악실 의자, 사진 등등

--- Assets/1.Scripts/1.Control/OpenBehavior.cs
+++ Assets/1.Scripts/1.Control/OpenBehavior.cs
@@ -2,53 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenBehavior : MonoBehaviour {
+public class OpenBehavior : MonoBehaviour
+{
 
-    public AudioClip openSfx;
-    public AudioClip closeSfx;
-
-    public float smooth = 2.0f;
-    public float openAngle = 90.0f;
-
+    private Animator _animator;
     private bool isOpen = false;
     private bool isEnter = false;
+    public AudioClip openSfx;
+    public AudioClip closeSfx;
+    private new AudioSource audio;
 
-    private Vector3 defaultRot;
-    private Vector3 openRot;
-
-	// Use this for initialization
-	void Start () {
-        defaultRot = transform.eulerAngles;
-        openRot = defaultRot + new Vector3(0, openAngle, 0);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (isOpen)
-        {
-            GetComponents<BoxCollider>()[0].enabled = false;
-            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, Time.deltaTime * smooth);
-        }
-        else
-        {
-            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, defaultRot, Time.deltaTime * smooth);
-            GetComponents<BoxCollider>()[0].enabled = true;
-        }
-
-        if (Input.GetKeyDown("f") && isEnter)
-        {
-            isOpen = !isOpen;
-
-            if(isOpen)
-            {
-                GetComponent<AudioSource>().PlayOneShot(openSfx);
-            }
-            else
-            {
-                GetComponent<AudioSource>().PlayOneShot(closeSfx);
-            }
-        }
-	}
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter(Collider col)
     {
@@ -60,9 +29,29 @@ public class OpenBehavior : MonoBehaviour {
 
     void OnTriggerExit(Collider col)
     {
-        if (col.tag == "Player")
+        if(col.tag == "Player")
         {
             isEnter = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( isEnter && Input.GetKeyDown("f") )
+        {
+            isOpen = !isOpen;   // 누를 때마다 상태를 바꾸는 거당
+            if (isOpen)
+            {
+                _animator.SetBool("isOpen", true);
+                audio.PlayOneShot(openSfx);
+                
+            }
+            else
+            {
+                _animator.SetBool("isOpen", false);
+                audio.PlayOneShot(closeSfx);
+            }
         }
     }
 }
