d70fb38a200fb63ef0b166079bcd3b5c6278ba19
BackYunki <37071344+BackYunki@users.noreply.github.com>
2018-04-26 19:13:33 +0900

    무시된파일 업로드

---
+++ Assets/1.Scripts/1.Control/PianoBehavior.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoBehavior : MonoBehaviour {
+
+    public AudioClip pianoSfx;
+
+    private bool isPlay = false;
+    private bool isEnter = false;
+
+    void Start()
+    {
+        GetComponent<AudioSource>().enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("f") && isEnter)
+        {
+            isPlay = !isPlay;
+
+            if (isPlay)
+            {
+                GetComponent<AudioSource>().enabled = true;
+                GetComponent<AudioSource>().PlayOneShot(pianoSfx);
+            }
+            else
+            {
+                GetComponent<AudioSource>().enabled = false;
+            }
+        }
+        else if (!isEnter)
+        {
+            GetComponent<AudioSource>().enabled = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if ( col.tag == "Player")
+        {
+            isEnter = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if( col.tag == "Player")
+        {
+            isEnter = false;
+        }
+    }
+}
