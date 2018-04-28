7e60260fa2f87e4fc177cf77765948419fe651d7
BackYunki <37071344+BackYunki@users.noreply.github.com>
2018-04-29 06:25:05 +0900

    헬리콥터 스크립트 잠시 꺼둠, 동건이꺼 임포트한거 컴파일 에러나서 삭제해둠, 스카이박스 밤으로 변경, 플레이어 랜턴 추가

---
+++ Assets/1.Scripts/1.Control/FadingUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadingUI : MonoBehaviour {
+
+    public void FadeMe()
+    {
+        StartCoroutine(DoFade());
+    }
+
+    IEnumerator DoFade()
+    {
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+        while(canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime / 2;
+            yield return null;
+        }
+        canvasGroup.interactable = false;
+        yield return null;
+    }
+}
---
+++ Assets/1.Scripts/1.Control/ZombieBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+public class ZombieBehavior : NetworkBehaviour {
+
+    private Transform playerTr;
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        StartCoroutine(GetPlayerPos());
+    }
+    IEnumerator GetPlayerPos()
+    {
+        while(playerTr == null)
+        {
+            yield return new WaitForSeconds(5.0f);
+            playerTr = GameObject.FindWithTag("Player").transform;
+            if (playerTr != null)
+            {
+                break;
+            }
+            continue;
+        }
+
+        agent.destination = playerTr.position;
+        yield return new WaitForSeconds(0.2f);
+    }
+}
