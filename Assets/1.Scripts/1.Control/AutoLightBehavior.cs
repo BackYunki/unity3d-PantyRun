0a8e93d6685dbf4cd9d54d50cd3d9a87b08d57b8
BackYunki <37071344+BackYunki@users.noreply.github.com>
2018-05-18 21:13:27 +0900

    플레이어 소켓 오브젝트 추가(SocketBehavior.cs 구현 바람), 헬기에 라이트 추가, 미술실 의자 변경, 누락 된 콜라이더 추가

--- Assets/1.Scripts/1.Control/AutoLightBehavior.cs
+++ Assets/1.Scripts/1.Control/AutoLightBehavior.cs
@@ -5,14 +5,19 @@ using UnityEngine.Networking;
 
 public class AutoLightBehavior : NetworkBehaviour
 {
-
+    // 조명 프리팹
     public Light bulb;
+    // 불 꺼지는 시간
     [Tooltip("불꺼지는 시간")]
     public float time = 5.0f;
+    // 트리거
     private Collider col;
+    // 측정기
     private float savetime = 0;
+    // 출입여부
     private bool isExit = false;
 
+    // 트리거 안에 들어와 있을 때
     void OnTriggerStay(Collider col)
     {
         this.col = col;
@@ -26,14 +31,14 @@ public class AutoLightBehavior : NetworkBehaviour
 
     void Update()
     {
-        if( isExit == false )
+        if (isExit == false)
         {
             if (col != null && col.GetComponentInChildren<MoveBehaviour>().IsMoving())
             {
                 savetime = 0;
                 bulb.enabled = true;
             }
-        }        
+        }
 
         savetime += Time.deltaTime;
 
