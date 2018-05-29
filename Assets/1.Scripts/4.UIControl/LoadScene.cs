b3776c3517244527a1455789059f04653e3aee78
BackYunki <37071344+BackYunki@users.noreply.github.com>
2018-05-29 23:51:44 +0900

    1. 승리, 패배 씬 완성 2. 로비 라이팅 버그 수정 3. 타이틀 화면 bgm 사운드 폴더에 넣어두었음~

---
+++ Assets/1.Scripts/4.UIControl/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadScene : MonoBehaviour {
+
+    [Tooltip("씬넘버입니다.")]
+    public int num = 0;
+
+	// Use this for initialization
+	public void OnClick () {
+        SceneManager.LoadScene(num);		
+	}
+}
---
+++ Assets/1.Scripts/4.UIControl/ResultBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/*
+ * 만든이: 백윤기
+ * 캔버스에 있는 UI 컴포넌트가 자신을 페이드인 하거나 페이드 아웃하는데 사용됩니다.
+ * 1. img_Mask 파일에 마스킹시킬 검정색 이미지를 등록하세요.
+ */
+
+public class ResultBehavior : MonoBehaviour {
+
+    [Tooltip("페이드인에 사용할 검정색 이미지를 준비해주세요.")]
+    public Image img_Mask;
+
+    [Tooltip("사용할 음악을 넣어주세요.")]
+    public AudioClip audio_Clip;
+    private AudioSource audio;
+
+    float fade_in = 0.0f;
+    float fade_out = 1.0f;
+    float fades = 1.0f;
+
+    bool is_fade_in = false;
+
+    void Start()
+    {
+        audio = gameObject.GetComponent<AudioSource>();
+        audio.PlayOneShot(audio_Clip);
+        InvokeRepeating("fade_In", 1, 0.1f);
+    }
+
+    private void fade_In()
+    {
+        if (fades > fade_in && !is_fade_in)
+        {
+            fades -= 0.1f;
+            img_Mask.color = new Color(0, 0, 0, fades);
+        }
+        else if (fades <= 0.0f)
+        {
+            is_fade_in = true;
+            // 버튼을 눌러야 하기 때문에 마스킹을 중지한다.
+            img_Mask.enabled = false;
+        }
+    }
+}
